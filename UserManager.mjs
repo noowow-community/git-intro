b5f2bce3d29dfa98662990bcbf706b1ce28252e0
Mr Stone <pierre.evens16@gmail.com>
2023-06-27 22:09:33 -0400

    Noo-2222: Corriger le nom

--- UserManager.mjs
+++ UserManager.mjs
@@ -1,18 +1,18 @@
 export function userManager(user) {
   this.createUser = function (user) {
-    this.databse.save(user)
+    this.database.save(user)
     console.log('Save user', JSON.stringify(user))
   }
 
   this.deleteUser = function (user) {
-    this.databse.delete(user)
+    this.database.delete(user)
   }
 
   this.updateUser = function (user) {
-    this.databse.update(user)
+    this.database.update(user)
   }
 
   this.listUser = function () {
-    this.databse.all()
+    this.database.all()
   }
 }
