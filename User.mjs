e79bce01e12242dc60e2df533d74fc2e952b6491
Mr Stone <pierre.evens16@gmail.com>
2023-06-27 21:11:21 -0400

    Revert "no-feat: Ajouter un champs languages"
    This reverts commit 7f36f3ddb960b08878171113c1ddd77b84c52199.

--- User.mjs
+++ User.mjs
@@ -1,3 +1,3 @@
 export function user() {
-  return { name: 'Stone', gender: 'male', age: 100, username: 'Evens', languages: ['Javscript', 'Swift', 'Java', 'PHP'] }
+  return { name: 'Stone', gender: 'male', age: 100, username: 'Evens' }
 }
