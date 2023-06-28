09c4ad37fa9f07b07bde10a5532469fb9510e8d6
Mr Stone <pierre.evens16@gmail.com>
2023-06-27 21:31:02 -0400

    Noo-1234: Ajouter la methode update

--- UserManager.mjs
+++ UserManager.mjs
@@ -2,4 +2,8 @@ export function userManager(user) {
   this.createUser = function (user) {
     this.databse.save(user)
   }
+
+  this.updateUser = function (user) {
+    this.databse.update(user)
+  }
 }
