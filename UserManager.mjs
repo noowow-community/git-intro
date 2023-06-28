b8d54f0dd9a84d443c2a7445284beded29da3cd6
Mr Stone <pierre.evens16@gmail.com>
2023-06-27 21:53:14 -0400

    Noo-1235: Ajouter une methode supprimer

--- UserManager.mjs
+++ UserManager.mjs
@@ -2,4 +2,8 @@ export function userManager(user) {
   this.createUser = function (user) {
     this.databse.save(user)
   }
+
+  this.deleteUser = function (user) {
+    this.databse.delete(user)
+  }
 }
